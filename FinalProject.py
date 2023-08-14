1bc0aec66b5a7b053b5e34b11d3303c26502016a
Pham Anh Quang <quangpade170281@fpt.edu.vn>
2023-08-14 19:48:50 +0700

    Update FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[32]:
+# In[5]:
 
 
 import tkinter as tk
 from tkinter import simpledialog, messagebox, filedialog
+from tkinter.ttk import Treeview
 import os
 
 class MenuItem:
